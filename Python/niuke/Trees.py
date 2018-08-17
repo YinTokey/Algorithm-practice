5672e840805d053489fa0a27e7c135b5d9e21e38
YinjianChen <YinjianChen02@gmail.com>
2018-08-17 08:14:28 +0800

    层次打印二叉树

--- Python/niuke/Trees.py
+++ Python/niuke/Trees.py
@@ -26,3 +26,69 @@ class Solution:
             result.append(node)
             node = node.right
         return result
+
+# 二叉树序列化与反序列化
+# 直接参照这个把 https://blog.csdn.net/u010005281/article/details/79787278
+class Solution:
+    def Serialize(self, root):
+        if not root:
+            return '#'
+        return str(root.val) + ',' + self.Serialize(root.left) + ',' + self.Serialize(root.right)
+    def Deserialize(self, s):
+        list = s.split(',')
+        return self.deserializeTree(list)
+    def deserializeTree(self,list):
+        if len(list) <= 0:
+            return None
+        val = list.pop(0)
+        root = None
+        if val != '#':
+            root = TreeNode(int(val))
+            root.left = self.deserializeTree(list)
+            root.right = self.deserializeTree(list)
+        return root
+
+# 从上到下分层输出二叉树，必须返回二维列表
+# 思路:因为要返回二维列表，所以需要对一般的层次遍历做一些修改。
+
+    # 返回二维列表[[1,2],[4,5]]
+    def Print(self, pRoot):
+        if pRoot == None:
+            return []
+        result,nodes = [],[pRoot]
+        while nodes :
+            curStack,nextStack = [],[]
+            for node in nodes:
+                curStack.append(node.val)
+                if node.left != None:
+                    nextStack.append(node.left)
+                if node.right != None:
+                    nextStack.append(node.right)
+            result.append(curStack)
+            nodes = nextStack
+        return result
+# 按之字形打印二叉树
+#  请实现一个函数按照之字形打印二叉树，即第一行按照从左到右的顺序打印，第二层按照从右至左的顺序打印，第三行按照从左到右的顺序打印，其他行以此类推。
+# 其实就是上面那一题，按照求余规则，对子数组翻转一下即可
+
+    def Print(self, pRoot):
+        result, nodes, index = [], [pRoot], 1
+        if pRoot == None:
+            return []
+        while nodes:
+            curStack, nextStack = [], []
+            for node in nodes:
+                curStack.append(node.val)
+                if node.left != None:
+                    nextStack.append(node.left)
+                if node.right != None:
+                    nextStack.append(node.right)
+            nodes = nextStack
+            result.append(curStack)
+        realResult = []
+        for arr in result:
+            if index % 2 == 0:
+                arr.reverse()
+            realResult.append(arr)
+            index += 1
+        return realResult
