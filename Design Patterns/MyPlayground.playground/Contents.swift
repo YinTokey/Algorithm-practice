74a48c5f8b4a0341fdf8e258a579093dc1c4e4d2
YinjianChen <YinjianChen02@gmail.com>
2018-12-01 14:47:51 +0800

    builder

--- Design Patterns/MyPlayground.playground/Contents.swift
+++ Design Patterns/MyPlayground.playground/Contents.swift
@@ -39,6 +39,62 @@ struct Product:Prototype{
  单例模式，封装共享资源，提供全局访问点
  */
 
+/*
+ 对象池模式 ，有些资料可能提及这个模式。是单例的扩展，管理一组可重用对象，比如tableview的cell复用机制
+ */
+
+/*
+ 工厂方法模式：定义创建对象的接口，让子类自己觉得要实例化哪一个工厂类，工厂模式使得创建过程延迟到子类进行
+ */
+
+
+/*
+ 抽象工厂模式：提供一个接口，能够创建一些列相关或相互依赖的对象。比如NSNumber的一些初始化方法 [NSNumber numberWithBool:YES],[NSNumber numberWithInt:1]
+ 抽象工厂里包含了很多工厂模式
+ */
+
+
+/*
+ 建造者模式：将对象的创建和配置分离，引入建造者类来配置对象.
+ */
+class StartBuck{
+    
+    init(milk:Bool,sugger:Bool){
+        
+    }
+}
+
+class CoffeBuilder{
+    private var sugger:Bool  = false
+    private var milk:Bool = false
+    
+    func setSugger(choice:Bool){
+        self.sugger = choice
+    }
+    
+    func setMilk(choice:Bool){
+        self.milk = choice
+    }
+    
+    func buildObject(name:String)->StartBuck{
+        return StartBuck(milk: milk, sugger: sugger)
+        
+    }
+}
+
+let builder = CoffeBuilder()
+builder.setMilk(choice: true)
+builder.setSugger(choice: false)
+
+let coffee = builder.buildObject(name: "10A")
+
+
+
+/*
+ =====================================================
+
+ */
+
 
 /*
  装饰者模式
