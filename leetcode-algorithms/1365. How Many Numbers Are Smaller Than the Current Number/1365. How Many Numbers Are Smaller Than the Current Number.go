2c8f8a02392e0e46b691f548ca0101b88807fb1f
YinjianChen <YinjianChen02@gmail.com>
2020-10-26 21:59:31 +0800

    1365

---
+++ leetcode-algorithms/1365. How Many Numbers Are Smaller Than the Current Number/1365. How Many Numbers Are Smaller Than the Current Number.go
@@ -0,0 +1,17 @@
+func smallerNumbersThanCurrent(nums []int) []int {
+	//基于计数排序
+	cnt := [101]int{}
+	for _, v := range nums {
+		cnt[v]++
+	}
+	for i := 0; i < 100; i++ {
+		cnt[i+1] += cnt[i]
+	}
+	ans := make([]int, len(nums))
+	for i, v := range nums {
+		if v > 0 {
+			ans[i] = cnt[v-1]
+		}
+	}
+	return ans
+}
