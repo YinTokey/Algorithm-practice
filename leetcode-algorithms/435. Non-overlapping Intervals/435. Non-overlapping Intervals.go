c891afa4bd99e34d44fdddf526b3bcb5dfe2980c
YinjianChen <YinjianChen02@gmail.com>
2020-10-29 19:30:14 +0800

    435

---
+++ leetcode-algorithms/435. Non-overlapping Intervals/435. Non-overlapping Intervals.go
@@ -0,0 +1,26 @@
+import "sort"
+
+func eraseOverlapIntervals(intervals [][]int) int {
+	if len(intervals) == 0 {
+		return 0
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] == intervals[j][0] {
+			return intervals[i][1] < intervals[j][1]
+		}
+		return intervals[i][0] < intervals[j][0]
+	})
+	count := 0
+	end := intervals[0][1]
+	for i := 1; i < len(intervals); i++ {
+		if intervals[i][0] >= end {
+			end = intervals[i][1]
+		} else {
+			if intervals[i][1] < end {
+				end = intervals[i][1]
+			}
+			count++
+		}
+	}
+	return count
+}
