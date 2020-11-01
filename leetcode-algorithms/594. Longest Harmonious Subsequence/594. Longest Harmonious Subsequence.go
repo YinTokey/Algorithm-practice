25beb16db27d32ef454428e0b2a5e6a18ab6412b
YinjianChen <YinjianChen02@gmail.com>
2020-11-01 10:18:29 +0800

    594

---
+++ leetcode-algorithms/594. Longest Harmonious Subsequence/594. Longest Harmonious Subsequence.go
@@ -0,0 +1,14 @@
+func findLHS(nums []int) int {
+	res := 0
+	count := make(map[int]int)
+	for _, num := range nums {
+		count[num]++
+		if count[num-1] != 0 && res < count[num-1]+count[num] {
+			res = count[num-1] + count[num]
+		}
+		if count[num+1] != 0 && res < count[num+1]+count[num] {
+			res = count[num+1] + count[num]
+		}
+	}
+	return res
+}
